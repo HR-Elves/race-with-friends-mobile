b0cbf0fd4f4398bcbc7196622c526838af3cb27d
James Falkoff <james.falkoff@gmail.com>
2017-02-28 19:17:18 -0800

    Feature: implement ability to fetch runs from server

--- src/components/MyRuns.js
+++ src/components/MyRuns.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View,
-  ListView
+  View
 } from 'react-native';
+
+import { Dimensions } from 'react-native';
 
 import james from '../../assets/presetChallenges/MarketSt3';
 import nick from '../../assets/presetChallenges/MarketSt4';
+
+import { ListItem, Subheader } from 'react-native-material-ui';
 
 export default class MyRuns extends Component {
   constructor(props) {
@@ -22,53 +25,91 @@ export default class MyRuns extends Component {
       this.setState ({
         runs: result
       });
-    });    
+    });
   }
 
+
+  // getRunsData(callback) {
+  //   let results = [];
+  //   results[0] = 
+  //     {
+  //       id: 0,
+  //       userid: 1234567890,
+  //       created: 'createdAtDate',
+  //       name: 'James Market St',
+  //       description: '100m dash on Market St',
+  //       length: james[james.length-1].distanceTotal,
+  //       duration: james[james.length-1].timeTotal,
+  //       data: james
+  //     };
+  //   results[1] = 
+  //      {
+  //       id: 1,
+  //       userid: 1234567890,
+  //       created: 'createdAtDate',
+  //       name: 'Nick Market St',
+  //       description: '100m dash on Market St',
+  //       length: nick[nick.length-1].distanceTotal,
+  //       duration: nick[nick.length-1].timeTotal,
+  //       data: nick
+  //     };     
+  //   callback(results);
+  // }
+
   getRunsData(callback) {
-    let results = [];
-    results[0] = 
+    let userId = '1233197366796721';
+    fetch('https://www.racewithfriends.tk:8000/users/' + userId + '/runs', 
       {
-        id: 0,
-        userid: 1234567890,
-        Created: 'createdAtDate',
-        name: 'James Market St',
-        description: '100m dash on Market St',
-        length: james[james.length-1].distanceTotal,
-        duration: james[james.length-1].timeTotal,
-        data: james
-      };
-    results[1] = 
-       {
-        id: 1,
-        userid: 1234567890,
-        Created: 'createdAtDate',
-        name: 'Nick Market St',
-        description: '100m dash on Market St',
-        length: nick[nick.length-1].distanceTotal,
-        duration: nick[nick.length-1].timeTotal,
-        data: nick
-      };     
-    callback(results);
-  }
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+      }
+      })
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJson) => {
+        callback(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }    
+
 
   render() {
     const styles = StyleSheet.create({
-       container: {
-        flex: 1
+      container: {
+        marginTop: 60,
+        flex: 1,
+        width: Dimensions.get('window').width
       },
+      listItem: {
+        width: Dimensions.get('window').width
+      }
     });
 
     return (
-      <View>
-        <Text>Hello?</Text>
-        <Text>{this.state.runs.length}</Text>
-        {
-          this.state.runs.map((run) => {
-            return (<Text>{run.name}</Text>);
-          })
-        }
+      <View style={styles.container}>
+        <Subheader text="My Runs" />
+          {
+            this.state.runs.map((run) => {
+             return (
+                <ListItem
+                  divider
+                  centerElement={{
+                            primaryText: run.name,
+                            secondaryText: run.created,
+                        }}
+                  onPress={() => {}}               
+                />
+              );
+            })
+          }        
       </View>
     );
   }
 }
+
+
--- src/components/StatsView.js
+++ src/components/StatsView.js
@@ -7,6 +7,7 @@ import {
 
 import BottomNavigation, { Tab } from 'react-native-material-bottom-navigation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { COLOR, ThemeProvider } from 'react-native-material-ui';
 
 import MyRuns from './MyRuns';
 
@@ -37,30 +38,43 @@ export default class StatsView extends Component {
       }
     });
 
+    const uiTheme = {
+        palette: {
+            primaryColor: COLOR.green500,
+        },
+        toolbar: {
+            container: {
+                height: 50,
+            },
+        },
+    };
+
     return (
-      <View style={styles.container}>
-        <View style={styles.center}>
-          {this.state.currentTab === 0 && <Text>My Stats</Text> }
-          {this.state.currentTab === 1 && <MyRuns /> }
+      <ThemeProvider uiTheme={uiTheme}>    
+        <View style={styles.container}>
+          <View style={styles.center}>
+            {this.state.currentTab === 0 && <Text>My Stats</Text> }
+            {this.state.currentTab === 1 && <MyRuns /> }
+          </View>
+          <BottomNavigation
+            labelColor="white"
+            rippleColor="white"
+            style={{ height: 56, elevation: 8, position: 'absolute', left: 0, bottom: 0, right: 0 }}
+            onTabChange={this.onTabChange.bind(this)}
+          >
+            <Tab
+              barBackgroundColor="#37474F"
+              label="My Stats"
+              icon={<Icon size={24} color="white" name="show-chart" />}
+            />
+            <Tab
+              barBackgroundColor="#00796B"
+              label="My Runs"
+              icon={<Icon size={24} color="white" name="directions-run" />}
+            />
+          </BottomNavigation>
         </View>
-        <BottomNavigation
-          labelColor="white"
-          rippleColor="white"
-          style={{ height: 56, elevation: 8, position: 'absolute', left: 0, bottom: 0, right: 0 }}
-          onTabChange={this.onTabChange.bind(this)}
-        >
-          <Tab
-            barBackgroundColor="#37474F"
-            label="My Stats"
-            icon={<Icon size={24} color="white" name="show-chart" />}
-          />
-          <Tab
-            barBackgroundColor="#00796B"
-            label="My Runs"
-            icon={<Icon size={24} color="white" name="directions-run" />}
-          />
-        </BottomNavigation>
-      </View>
+      </ThemeProvider>
     );
   }
 }
