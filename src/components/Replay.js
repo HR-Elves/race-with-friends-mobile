cfd1ad8518d3e2da7ae1ddfb4c80da8192af9668
sscchan <stephen.s.c.chan@gmail.com>
2017-03-08 16:09:07 -0800

    add toolbar and bottom button for replay

--- src/components/Replay.js
+++ src/components/Replay.js
@@ -4,10 +4,14 @@ import {
   StyleSheet,
   Text,
   View,
-  Button,
+  // Button,
 } from 'react-native';
-import {Vibration} from 'react-native';
+
+import { Vibration, Dimensions } from 'react-native';
 import ModalDropdown from 'react-native-modal-dropdown';
+import { ThemeProvider, Toolbar, Button } from 'react-native-material-ui'
+import uiTheme from './uiTheme.js'
+
 import Tts from 'react-native-tts';
 import _ from 'lodash';
 
@@ -409,6 +413,12 @@ export default class Replay extends Component {
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
       },
+      list: {
+        marginTop: 60,
+        marginBottom: 56,
+        flex: 1,
+        width: Dimensions.get('window').width
+      },      
       welcome: {
         fontSize: 20,
         textAlign: 'center',
@@ -434,72 +444,81 @@ export default class Replay extends Component {
     });
 
     return (
-      <View style={styles.container}>
-        <Text style={{marginBottom: 0, marginTop: 50}}>Select Racers:</Text>
-        <View style={styles.dropdown}>
-          <ModalDropdown
-            options={['Presets', 'My Runs', 'Challenges']}
-            onSelect={this.onPickPlayerType.bind(this)}
-            textStyle={{fontSize: 18}}
-            defaultValue='Presets'
-            // style={{marginTop: 25}}
-          />
-          <Text style={{fontSize: 18, marginRight: 10}}>:</Text>
-          <ModalDropdown
-            options={this.state.playerSetup.options}
-            onSelect={this.onPickPlayer.bind(this)}
-            textStyle={{fontSize: 24}}
-            defaultValue='worldRecordRaceWalk100m'
-            // style={{marginTop: 25}}
-          />
+      <ThemeProvider uiTheme={uiTheme}>      
+        <View style={styles.container}>
+          <View style={styles.list}>
+            <Toolbar centerElement="Race Replay" />      
+            <Text style={{marginBottom: 0, marginTop: 50}}>Select Racers:</Text>
+            <View style={styles.dropdown}>
+              <ModalDropdown
+                options={['Presets', 'My Runs', 'Challenges']}
+                onSelect={this.onPickPlayerType.bind(this)}
+                textStyle={{fontSize: 18}}
+                defaultValue='Presets'
+                // style={{marginTop: 25}}
+              />
+              <Text style={{fontSize: 18, marginRight: 10}}>:</Text>
+              <ModalDropdown
+                options={this.state.playerSetup.options}
+                onSelect={this.onPickPlayer.bind(this)}
+                textStyle={{fontSize: 24}}
+                defaultValue='worldRecordRaceWalk100m'
+                // style={{marginTop: 25}}
+              />
+            </View>
+            <Text style={{
+              fontSize: 20,
+              // marginTop: 10,
+              // marginBottom: 10
+            }}>VS</Text>
+            <View style={styles.dropdown}>
+              <ModalDropdown
+                options={['Presets', 'My Runs', 'Challenges']}
+                onSelect={this.onPickOpponentType.bind(this)}
+                textStyle={{fontSize: 18}}
+                defaultValue='Presets'
+                // style={{marginTop: 25}}
+              />
+              <Text style={{fontSize: 18, marginRight: 10}}>:</Text>
+              <ModalDropdown
+                options={this.state.opponentSetup.options}
+                onSelect={this.onPickOpponent.bind(this)}
+                textStyle={{fontSize: 24}}
+                defaultValue='worldRecordRaceWalk100m'
+                // style={{marginTop: 25}}
+              />
+            </View>
+            <RaceStatus
+              status={this.state.raceStatus}
+              playerName={'Player'}
+              opponentName={'Opponent'}
+              playersSwapped={this.state.playersSwapped}
+            />
+            <RaceProgress progress={this.state.progress} />
+
+            <View style={styles.buttons}>
+            {/*
+              <Button
+                onPress={this.onPlay.bind(this)}
+                title='Play'
+                color='red'
+              />
+              <Button
+                onPress={this.onPause.bind(this)}
+                title='Pause'
+                color='blue'
+              />
+              <Button
+                onPress={this.onReset.bind(this)}
+                title='Reset'
+                color='green'
+              />
+            */}
+            </View>
+          </View>
+          <Button style={{container:{width: Dimensions.get('window').width}}} raised primary text="Play" onPress={this.onPlay.bind(this)} />
         </View>
-        <Text style={{
-          fontSize: 20,
-          // marginTop: 10,
-          // marginBottom: 10
-        }}>VS</Text>
-        <View style={styles.dropdown}>
-          <ModalDropdown
-            options={['Presets', 'My Runs', 'Challenges']}
-            onSelect={this.onPickOpponentType.bind(this)}
-            textStyle={{fontSize: 18}}
-            defaultValue='Presets'
-            // style={{marginTop: 25}}
-          />
-          <Text style={{fontSize: 18, marginRight: 10}}>:</Text>
-          <ModalDropdown
-            options={this.state.opponentSetup.options}
-            onSelect={this.onPickOpponent.bind(this)}
-            textStyle={{fontSize: 24}}
-            defaultValue='worldRecordRaceWalk100m'
-            // style={{marginTop: 25}}
-          />
-        </View>
-        <RaceStatus
-          status={this.state.raceStatus}
-          playerName={'Player'}
-          opponentName={'Opponent'}
-          playersSwapped={this.state.playersSwapped}
-        />
-        <RaceProgress progress={this.state.progress} />
-        <View style={styles.buttons}>
-          <Button
-            onPress={this.onPlay.bind(this)}
-            title='Play'
-            color='red'
-          />
-          <Button
-            onPress={this.onPause.bind(this)}
-            title='Pause'
-            color='blue'
-          />
-          <Button
-            onPress={this.onReset.bind(this)}
-            title='Reset'
-            color='green'
-          />
-        </View>
-      </View>
+      </ThemeProvider>
     );
   }
 }
